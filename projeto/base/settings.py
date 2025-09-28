340306e3134d7d4aa132a87008c5fa76d2475a27
ViniciusCosmos <viniciuscosmosilva@gmail.com>
2025-09-28 14:47:50 -0300

    Consertando Requirements e versoes compativeis

--- projeto/base/settings.py
+++ projeto/base/settings.py
@@ -98,4 +98,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Whitenoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
