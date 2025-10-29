e05763607f21792aaa73140ebe4ecb4b92e7ff5f
ViniciusCosmos <viniciuscosmosilva@gmail.com>
2025-10-29 13:39:06 -0300

    Ajustando estrutura

--- base/wsgi.py
+++ base/wsgi.py
@@ -5,5 +5,5 @@ import sys
 sys.path.append('/app/projeto')
 sys.path.append('/app')
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto.base.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
 application = get_wsgi_application()
